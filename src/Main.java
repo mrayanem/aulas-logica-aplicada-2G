a14deb5116766216f74201ebd368ba1ce5b6c94a
mrayanem <mmarquesraioane586@gmail.com>
2023-05-05 11:04:24 -0300

    classe pessoa

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         //aula introdutória de orientação a objeto
+
+
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,6 @@
+public class Pessoa {
+
+    //atributo
+    String nomes;
+
+}
