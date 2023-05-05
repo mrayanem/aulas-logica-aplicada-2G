e2ef3474f6d8764e8c2538bb1ac098a6bcce840f
programadormovel <programadormovel@gmail.com>
2023-05-05 11:04:23 -0300

    Classe Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,21 +1,4 @@
-public class Pessoa extends Animal {
-    // Atributos da classe
-    String sobrenome;
-    // Métodos da classe
-
-    // Sobrecarga do método
-    public void falar(){
-        System.out.println("falei");
-    }
-    public void falar(String volume){
-        System.out.println("falei " + volume);
-    }
-    public String falar(String volume,
-                        String tom){
-        return "falei " + volume + " e " + tom;
-    }
-    // Sobrescrita de método
-    public void comer(){
-        System.out.println("Comer como pessoa");
-    }
+public class Pessoa {
+    // Atributo
+    String nome;
 }
