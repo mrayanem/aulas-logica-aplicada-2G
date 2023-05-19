c98bc18398863c02c34e59321745aa85fa90bd46
programadormovel <programadormovel@gmail.com>
2023-05-19 10:03:06 -0300

    atividade

--- src/Animal.java
+++ src/Animal.java
@@ -1,15 +1,6 @@
 public class Animal {
-    // Atributos
-    String especie;
-    String porte;
-    int idade;
-    String raca;
-    String corPelo;
-    String corOlhoDireito;
-    String nome;
-    // Métodos
-    public void comer(){
-        System.out.println("Animal comeu");
-    }
+    // Definir 2 ou mais atributos
 
+    // Definir 1 ou mais métodos
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -14,25 +14,7 @@ public class Main {
         // Definir comportamento do objeto
         qualquer.falar();
         adao.falar("baixo");
-        // Criar mais objetos
-        // Declarar objetos
-        Pessoa rainha;
-        // Instalar objetos
-        rainha = new Pessoa();
-        //Definir formato do objeto
-        rainha.nome = "Tina";
-        rainha.sobrenome = "Tunner";
-        // Definir comportamento
-        rainha.falar();
-        rainha.falar("alto");
-        System.out.println(rainha.falar("alto", "grave"));
 
-        Animal cao;
-        cao = new Animal();
-        cao.nome = "Pluto";
-        cao.comer();
-
-        rainha.comer();
 
 
 
