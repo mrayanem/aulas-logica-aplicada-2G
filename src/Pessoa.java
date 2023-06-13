2e94101ba291059e4cff5374d34172a0895aab3d
Rayane Marques <115513080+mrayanem@users.noreply.github.com>
2023-06-13 11:49:32 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,21 @@
-public class Pessoa {
+public class Pessoa extends Animal {
+    // Atributos da classe
+    String sobrenome;
+    // Métodos da classe
 
-    //atributo
-    String nomes;
-
+    // Sobrecarga do método
+    public void falar(){
+        System.out.println("falei");
+    }
+    public void falar(String volume){
+        System.out.println("falei " + volume);
+    }
+    public String falar(String volume,
+                        String tom){
+        return "falei " + volume + " e " + tom;
+    }
+    // Sobrescrita de método
+    public void comer(){
+        System.out.println("Comer como pessoa");
+    }
 }
