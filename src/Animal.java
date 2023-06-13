c326c99b661efc1a6ca5ad558afd321dff76a0f8
mrayanem <mmarquesraioane586@gmail.com>
2023-06-13 11:45:41 -0300

    att

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    // Atributos
+    String especie;
+    String porte;
+    int idade;
+    String raca;
+    String corPelo;
+    String corOlhoDireito;
+    String nome;
+    // Métodos
+    public void comer(){
+        System.out.println("Animal comeu");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
 public class Main {
     public static void main(String[] args) {
+        // Aula introdutória de Orientação à Objetos
+        // Criar objetos
+        Pessoa adao = new Pessoa();
+        // Declaração de objeto
+        Pessoa qualquer;
+        // Instanciação do objeto
+        qualquer = new Pessoa();
+        Pessoa adulta = new Pessoa();
+        // Definir forma do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        // Definir comportamento do objeto
+        qualquer.falar();
+        adao.falar("baixo");
+        // Criar mais objetos
+        // Declarar objetos
+        Pessoa rainha;
+        // Instalar objetos
+        rainha = new Pessoa();
+        //Definir formato do objeto
+        rainha.nome = "Tina";
+        rainha.sobrenome = "Tunner";
+        // Definir comportamento
+        rainha.falar();
+        rainha.falar("alto");
+        System.out.println(rainha.falar("alto", "grave"));
 
-        //aula introdutória de orientação a objeto
+        Animal cao;
+        cao = new Animal();
+        cao.nome = "Pluto";
+        cao.comer();
+
+        rainha.comer();
 
 
 
